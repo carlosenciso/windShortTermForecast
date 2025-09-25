b7ff1a3d94b49e75046afeac43ffd6ceb9b201ad
Carlos Enciso <carlos.enciso.o@gmail.com>
2025-09-24 19:32:33 -0500

    change port

--- app/app.py
+++ app/app.py
@@ -398,4 +398,4 @@ def update_wind_plot(windfarm_value, dark):
 # Main Execution
 # =======================
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=8050, debug=False)
+    app.run(host="0.0.0.0", port=8050, debug=False)
